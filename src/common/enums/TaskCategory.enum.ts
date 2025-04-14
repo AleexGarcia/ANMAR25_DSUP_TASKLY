49d89c9a0d5a48b4be804f29e9a28198e3264e9d
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-14 15:05:04 -0300

    chore: create enums for task management (category, priority, status)

---
+++ src/common/enums/TaskCategory.enum.ts
@@ -0,0 +1,11 @@
+export enum TaskCategory {
+  bug_fixing = 1,
+  feature = 2,
+  testing = 3,
+  documentation = 4,
+  refactoring = 5,
+  security = 6,
+  configuration_management = 7,
+  code_review = 8,
+  optmization = 9,
+}
---
+++ src/common/enums/TaskStatus.enum.ts
@@ -0,0 +1,5 @@
+export enum TaskStatus {
+  todo = 0,
+  in_progress = 1,
+  done = 2,
+}
