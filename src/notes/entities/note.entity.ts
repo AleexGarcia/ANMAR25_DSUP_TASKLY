1ac83f56ba316a49060baa2a643b1a0b1dd2afaf
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-14 15:06:03 -0300

    feat: create note and task entities, add relationships

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,1 +1,12 @@
-export class Note {}
+import { Task } from 'src/tasks/entities/task.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('notes')
+export class Note {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @ManyToOne(() => Task, (task) => task.notes)
+  task_id: Task;
+  @Column({ nullable: false })
+  content: string;
+}
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,18 @@
-export class Task {}
+import { TaskCategory } from 'src/common/enums/TaskCategory.enum';
+import { TaskPriority } from 'src/common/enums/TaskPriority.enum';
+import { TaskStatus } from 'src/common/enums/TaskStatus.enum';
+import { Note } from 'src/notes/entities/note.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+@Entity('tasks')
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ enum: TaskStatus, enumName: 'TaskStatus' })
+  status: TaskStatus;
+  @Column({ enum: TaskPriority, enumName: 'TaskPriority' })
+  priority: TaskPriority;
+  @Column({ enum: TaskCategory, enumName: 'TaskCategory' })
+  category: TaskCategory;
+  @OneToMany(() => Note, (note) => note.task_id)
+  notes: Note[];
+}
