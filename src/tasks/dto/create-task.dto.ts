16e47a46201e08ba8ef24898ad868b07c9e305c0
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-14 18:05:33 -0300

    refactor: add custom error messages to task schemas and fix implementation issues

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -3,12 +3,10 @@ import { TaskPriority } from 'src/common/enums/TaskPriority.enum';
 import { TaskStatus } from 'src/common/enums/TaskStatus.enum';
 import { z } from 'zod';
 
-export const createTaskSchema = z
-  .object({
-    status: z.nativeEnum(TaskStatus).optional(),
-    priority: z.nativeEnum(TaskPriority).optional(),
-    category: z.nativeEnum(TaskCategory),
-  })
-  .required();
+export const createTaskSchema = z.object({
+  category: z.nativeEnum(TaskCategory, { message: 'category is required' }),
+  status: z.nativeEnum(TaskStatus).optional(),
+  priority: z.nativeEnum(TaskPriority).optional(),
+});
 
 export type CreateTaskDto = z.infer<typeof createTaskSchema>;
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -4,10 +4,9 @@ import { TaskStatus } from 'src/common/enums/TaskStatus.enum';
 import { z } from 'zod';
 
 export const updateTaskSchema = z.object({
-  status: z.nativeEnum(TaskStatus),
-  priority: z.nativeEnum(TaskPriority),
-  category: z.nativeEnum(TaskCategory),
-  id: z.number(),
+  status: z.nativeEnum(TaskStatus, { message: 'status is required' }),
+  priority: z.nativeEnum(TaskPriority, { message: 'priority is required' }),
+  category: z.nativeEnum(TaskCategory, { message: 'category is required' }),
 });
 
 export type UpdateTaskDto = z.infer<typeof updateTaskSchema>;
