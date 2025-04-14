d947f16f6e3186840fb5b531a632ea2c13a18399
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-14 18:00:15 -0300

    chore: register existing entities in TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { NotesModule } from './notes/notes.module';
+import { Task } from './tasks/entities/task.entity';
+import { Note } from './notes/entities/note.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db_taskly',
-      entities: [],
+      entities: [Task, Note],
       synchronize: true,
     }),
     TasksModule,
