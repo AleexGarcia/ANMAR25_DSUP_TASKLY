a6ecd6c59faf9fdab95f02c35bc31a09965acbcb
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-15 16:56:21 -0300

    refactor: update TypeORM setup to use migrations and create additional file with datasource options

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { NotesModule } from './notes/notes.module';
-import { Task } from './tasks/entities/task.entity';
-import { Note } from './notes/entities/note.entity';
+import { dataSourceOptions } from './db/data-source';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db_taskly',
-      entities: [Task, Note],
-      synchronize: true,
-    }),
-    TasksModule,
-    NotesModule,
-  ],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), TasksModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { join } from 'path';
+const pathMigrations = join(__dirname, '../migrations/*.{ts,js}');
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'sqlite',
+  database: 'db_taskly.db',
+  synchronize: true,
+  entities: [],
+  migrations: [pathMigrations],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
