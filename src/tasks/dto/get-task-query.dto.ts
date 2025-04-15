bc2dd0d68478c9ff34f15bba611fb6d43800aa91
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-15 16:57:37 -0300

    refactor: adjust get-task-query to use string for category (TaskCategory enum updated)

--- src/tasks/dto/get-task-query.dto.ts
+++ src/tasks/dto/get-task-query.dto.ts
@@ -4,11 +4,7 @@ import { TaskStatus } from 'src/common/enums/TaskStatus.enum';
 import { z } from 'zod';
 
 export const getTaskQuerySchema = z.object({
-  category: z
-    .string()
-    .transform((val) => parseInt(val))
-    .pipe(z.nativeEnum(TaskCategory))
-    .optional(),
+  category: z.nativeEnum(TaskCategory).optional(),
   priority: z.nativeEnum(TaskPriority).optional(),
   status: z.nativeEnum(TaskStatus).optional(),
 });
