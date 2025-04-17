845d47bad94fa4667087ec690f1050b8ec1c1fdd
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-17 14:34:42 -0300

    chore: import NoteRepository in TasksModule for use in TasksService delete method

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
+import { Note } from '../notes/entities/note.entity';
 
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), TypeOrmModule.forFeature([Note])],
 })
 export class TasksModule {}
