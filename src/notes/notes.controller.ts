0205e11c3c184965ef8c55303a67ce5059ddec41
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-17 14:28:11 -0300

    feat: add taskId param and ID validation and handler exception not found to noteservice

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -6,50 +6,55 @@ import {
   Param,
   Delete,
   Put,
-  UsePipes,
-  NotFoundException,
   HttpCode,
 } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { CreateNoteDto, createNoteSchema } from './dto/create-note.dto';
 import { UpdateNoteDto, updateNoteSchema } from './dto/update-note.dto';
 import { ZodValidationPipe } from '../common/pipes/zod-validation/zod-validation.pipe';
-import { DeleteResult } from 'typeorm';
+import { ParamTaskIdDto, paramTaskIdSchema } from './dto/param-taskid-dto';
+import { ParamIdDto, paramIdSchema } from './dto/param-id.dto';
 
 @Controller()
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
   @Post('tasks/:taskId/notes')
-  @UsePipes(new ZodValidationPipe(createNoteSchema))
-  async create(@Body() createNoteDto: CreateNoteDto) {
-    return this.notesService.create(createNoteDto);
+  async create(
+    @Param(new ZodValidationPipe(paramTaskIdSchema)) { taskId }: ParamTaskIdDto,
+    @Body(new ZodValidationPipe(createNoteSchema))
+    createNoteDto: CreateNoteDto,
+  ) {
+    return this.notesService.create(taskId, createNoteDto);
   }
 
   @Get('tasks/:taskId/notes')
-  async findAll(@Param('taskId') taskId: string) {
-    return this.notesService.findAll(+taskId);
+  async findAll(
+    @Param(new ZodValidationPipe(paramTaskIdSchema)) { taskId }: ParamTaskIdDto,
+  ) {
+    return this.notesService.findAll(taskId);
   }
 
   @Get('notes/:id')
-  async findOne(@Param('id') id: string) {
-    return this.notesService.findOne(+id);
+  async findOne(
+    @Param(new ZodValidationPipe(paramIdSchema)) { id }: ParamIdDto,
+  ) {
+    return this.notesService.findOne(id);
   }
 
   @Put('notes/:id')
   async update(
-    @Param('id') id: string,
+    @Param(new ZodValidationPipe(paramIdSchema)) { id }: ParamIdDto,
     @Body(new ZodValidationPipe(updateNoteSchema)) updateNoteDto: UpdateNoteDto,
   ) {
-    return this.notesService.update(+id, updateNoteDto);
+    return this.notesService.update(id, updateNoteDto);
   }
 
   @Delete('notes/:id')
   @HttpCode(204)
-  async remove(@Param('id') id: string) {
-    const deleteResult: DeleteResult = await this.notesService.remove(+id);
-    if (deleteResult && deleteResult.affected === 0) {
-      throw new NotFoundException('Note not Found');
-    }
+  async remove(
+    @Param(new ZodValidationPipe(paramIdSchema)) { id }: ParamIdDto,
+  ) {
+    return this.notesService.remove(id);
   }
 }
